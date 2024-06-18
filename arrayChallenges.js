f8e1b01a022b799da43cf21f50f5293ebe778c57
Ireme-Nyawe <ishimwevalens@gmail.com>
2024-06-18 15:17:57 +0200

    split character

--- arrayChallenges.js
+++ arrayChallenges.js
@@ -5,15 +5,17 @@ var str = "Hello World";
 var result = str.split(" ");
 console.log(result); // Output: ["Hello", "World"]
 
-
 // Using a Comma as Separator:
 str = "apple,banana,cherry";
 result = str.split(",");
 console.log(result); // Output: ["apple", "banana", "cherry"]
 
-// split with limit 
- str = "one,two,three,four,five";
- result = str.split(",", 3);
+// split with limit
+str = "one,two,three,four,five";
+result = str.split(",", 3);
 console.log(result); // Output: ["one", "two", "three"]
 
-
+//  spliting character
+str = "Hello";
+result = str.split("");
+console.log(result); // Output: ["H", "e", "l", "l", "o"]
