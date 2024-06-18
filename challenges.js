91ffeac88e4e08f2657a0a30175380c1930f2ec9
Ireme-Nyawe <ishimwevalens@gmail.com>
2024-06-18 15:07:09 +0200

    add first example

--- challenges.js
+++ challenges.js
@@ -1,3 +1,6 @@
 /** we can change the string into array using split()
  * string.split(separator, limit)
  */
+const str = "Hello World";
+const result = str.split(" ");
+console.log(result); // Output: ["Hello", "World"]
